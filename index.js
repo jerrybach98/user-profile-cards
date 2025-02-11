05d00a6f4c1c1a7834fde80aa52b955ad1d58434
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2025-02-10 20:34:58 -0800

    Set up project directory and add pseudocode

---
+++ index.js
@@ -0,0 +1,26 @@
+/* Pseudo
+Empty Parent container
+Add each user profile as another container (Avatar, name, email, country, flag icon, button)
+Use Fetch from external api
+Parse the required components into divs and update dynamically with DOM 
+
+Style with tailwind
+Add conistent and dynamic spacing using flexbox or grid between profile card containers
+
+flag state using vanilla or look into framework to manage state
+UI for loading, success, and error states
+
+flag logic
+on click event for flag icon / change color or image
+send post request with user data through JSON 
+Test post request through local endpoint
+Post to given Directus backend API
+disable duplicate flagging
+
+Extras 
+Dark mode support with tailwind
+Add micro interacions (hover animation)
+Lazy loading for images
+devounced api call 
+Keyboard navigation
+*/
